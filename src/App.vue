28116cb3441c8526d2738d8aad582dbbdc0c690f
benthehorn <benhorn13@googlemail.com>
2019-01-08 13:20:04 +0100

    firdt commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <to-do-list></to-do-list>
+  </div>
+</template>
+
+<script>
+import ToDoList from './components/ToDoList'
+
+export default {
+  components: {
+    ToDoList,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
